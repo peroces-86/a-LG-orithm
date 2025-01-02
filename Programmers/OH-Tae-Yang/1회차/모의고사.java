35bdb3d42b7da07b79e615fa4951164b8fa5a50d
오썬 <tyo1012@naver.com>
2025-01-02 17:12:48 +0900

    Programmers_1회차_오태양

---
+++ Programmers/OH-Tae-Yang/1회차/모의고사.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        int [] a = {1,2,3,4,5};
+        int [] b = {2,1,2,3,2,4,2,5};
+        int [] c = {3,3,1,1,2,2,4,4,5,5};
+
+        int [] score = new int[3];
+
+        for(int i = 0; i<answers.length; i++) {
+            if(answers[i] == a[i%a.length]) {
+                score[0] ++;
+            }
+            if(answers[i] == b[i%b.length]) {
+                score[1] ++;
+            }
+            if(answers[i] == c[i%c.length]) {
+                score[2] ++;
+            }
+        }
+
+        ArrayList<Integer> list = new ArrayList<>();
+
+        int max = Math.max(score[0], Math.max(score[1], score[2]));
+        for(int i=0; i<score.length; i++) {
+            if(max == score[i]) {
+                list.add(i+1);
+            }
+        }
+
+        int [] answer = new int[list.size()];
+        int n = 0;
+        for(int i : list) {
+            answer[n++] =i;
+        }
+        return answer;
+    }
+}
