0e93095550780761911431ca80f1d896278266f5
Oh-Tae-Yang <tyo1012@naver.com>
2025-02-16 11:51:56 +0000

    COMMITTED BY Oh-Tae-Yang: Processed 42747. H－Index

--- Programmers/Oh-Tae-Yang/42747. H－Index/H－Index.java
+++ Programmers/Oh-Tae-Yang/42747. H－Index/H－Index.java
@@ -5,17 +5,28 @@ import java.util.*;
 2. 정렬한 후, 인덱스 위치에 따라서 해당 위치보다는 전부 큰 값이 있는 것임을 판단
 3. 인용된 횟수를 확인하며 처음으로 H번 이상 인용된 데이터가 나오면 break
 */
+
+
+import java.util.*;
+
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
+        int n = citations.length;
+        
         Arrays.sort(citations);
-        for (int i = 0; i <citations.length; i++) {
-            int h = citations.length - i;
-            if (citations[i] >= h) {
+        
+        for(int i = 0; i < n; i++) {
+            // i가 작을 수록 최대 H가 구해진다
+            int h = n - i;
+            
+            // 오름차순 정렬이므로 citations[i]가 h번 이상 인용된 경우, i+1 부터는 모두 h번 이상 인용된다.
+            if(citations[i] >= h) {
                 answer = h;
                 break;
             }
         }
+        
         return answer;
     }
 }
