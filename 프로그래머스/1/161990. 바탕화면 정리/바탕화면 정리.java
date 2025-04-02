9c512ce539a3d93f24f41fa433ac05b02aa35b73
pero <leesanghoon1@naver.com>
2025-04-02 16:05:04 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.29 ms, Memory: 87 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] solution(String[] wallpaper) {
+
+        int minRow = wallpaper.length;
+        int minCol = wallpaper[0].length();
+        int maxRow = 0;
+        int maxCol = 0;
+
+        for(int i = 0; i < wallpaper.length; i++){
+            for(int j = 0; j < wallpaper[0].length(); j++){
+                if(wallpaper[i].charAt(j) == '#'){
+                    minRow = Math.min(minRow, i);
+                    minCol = Math.min(minCol, j);
+                    maxRow = Math.max(maxRow, i);
+                    maxCol = Math.max(maxCol, j);
+                }
+            }
+        }
+
+        return new int[]{minRow, minCol, maxRow + 1, maxCol+1};
+    }
+}
